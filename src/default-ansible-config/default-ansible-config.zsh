f76f7ba0868e39e72dae0c0a047419f4f2618c09
Leonid Korokh <lkorokh@gmail.com>
2025-11-05 07:37:51 +0000

    feat(ansible-config): implement Ansible configuration script with Mitogen support
    - Added default-ansible-config.zsh for Ansible setup
    - Configured vault password handling and Mitogen strategy
    - Updated install.sh to copy the new configuration script

---
+++ src/default-ansible-config/default-ansible-config.zsh
@@ -0,0 +1,38 @@
+#!/usr/bin/env zsh
+
+set -e -u -o pipefail
+#sudo chown -R "$(whoami):$(whoami)" /home/vscode/.ssh
+#chmod 700 /home/vscode/.ssh
+#chmod 600 /home/vscode/.ssh/*
+
+pipx install -q ansible-lint
+
+local VAULT_PASS_DIR="$HOME/bin"
+local VAULT_PASS_FILE="$VAULT_PASS_DIR/ansible-vault-pass.sh"
+local ANSIBLE_CFG_FILE="$HOME/.ansible.cfg"
+
+if [ -f $VAULT_PASS_FILE ]; then
+    chmod +x $VAULT_PASS_FILE
+else
+    echo "Warning: $VAULT_PASS_FILE not found, creating it."
+    read -s "VAULT_PASS?Enter Ansible Vault password: "
+    mkdir -p "$VAULT_PASS_DIR"
+    printf "#!/bin/sh\necho -n '%s'\n" "$VAULT_PASS" > "$VAULT_PASS_FILE"
+    chmod +x "$VAULT_PASS_FILE"
+fi
+echo ""
+
+local ANSIBLE_CONFIGURED=$(test -f "$ANSIBLE_CFG_FILE" && grep -F "vault_password_file=" "$ANSIBLE_CFG_FILE")
+if [ -z "$ANSIBLE_CONFIGURED" ]; then
+    echo "Ansible configuration located at $ANSIBLE_CFG_FILE"
+    echo "[defaults]" >> $ANSIBLE_CFG_FILE
+    echo "vault_password_file=$VAULT_PASS_FILE" >> $ANSIBLE_CFG_FILE
+    echo "callbacks_enabled = ansible.posix.profile_tasks, ansible.posix.timer" >> $ANSIBLE_CFG_FILE
+    echo "Enabling Mitogen for Ansible."
+    local venvs=$(pipx list | grep -F "venvs are in " | awk '{print $4;}')
+    local python_version=$(python3 --version | awk '{print $2;}' | cut -d. -f1,2)
+    echo "strategy_plugins=$venvs/ansible-core/lib/python$python_version/site-packages/ansible_mitogen/plugins/strategy" >> $ANSIBLE_CFG_FILE
+    echo "strategy=mitogen_linear" >> $ANSIBLE_CFG_FILE
+else
+    echo "Ansible is already configured."
+fi
